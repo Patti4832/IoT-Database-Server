696685dd53ead1ce35a1d3508ba55e153725a99e
Patti4832 <patti4832@gmx.de>
2018-07-07 15:43:02 +0200

    Add files via upload

---
+++ Source/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoT_Database_Server
+{
+    class Program
+    {
+        static void Main(string[] args)     //port in args[0]
+        {
+            Updater.Updater u = new Updater.Updater();
+            u.updateURL = "https://raw.githubusercontent.com/Patti4832/IoT-Database-Server/master/Update/";
+            u.files = 1;
+            u.fileNames = new string[] { "IoT Database Server.exe" };
+            u.versions = new int[] { 2 };
+            u.CheckForUpdate();
+            if (u.updateable)
+                u.InstallUpdate();
+            else
+            {
+                int port = 80;
+                if (args.Length == 1)
+                {
+                    try
+                    {
+                        port = int.Parse(args[0]);
+                    }
+                    catch { }
+                }
+
+                Log.Info("Starting IoT Databse Server ...");
+
+                Database.Init();
+
+                Config.debug = true;
+                Server s1 = new Server(port);
+                s1.Start();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Source/Updater.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Updater
+{
+    public class Updater
+    {
+        public int version = 1;
+
+        public int files = 1;
+        public bool[] updateAvailable;
+        public bool updateable = false;
+        public int[] versions;
+        public string[] fileNames;
+        public string updateURL = "";
+
+        WebClient client = new WebClient();
+
+        public void CheckForUpdate()
+        {
+            updateAvailable = new bool[versions.Length];
+
+            string updateFile = client.DownloadString(updateURL + "versions.txt");
+
+            int[] fileVersions = new int[4];
+
+            int tmpCount = 0;
+
+            foreach (string s in updateFile.Replace("\n", "").Replace("\r", "").Split(new string[] { ";" }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                if (versions[tmpCount] < int.Parse(s.Split(new string[] { "=" }, StringSplitOptions.RemoveEmptyEntries)[1]))
+                    updateAvailable[tmpCount] = true;
+                else
+                    updateAvailable[tmpCount] = false;
+                tmpCount++;
+            }
+
+
+            for (int i = 0; i < tmpCount; i++)
+            {
+                if (updateAvailable[i])
+                    updateable = true;
+            }
+        }
+
+        public void InstallUpdate()
+        {
+            if (!Directory.Exists("Update"))
+                Directory.CreateDirectory("Update");
+
+            for (int i = 0; i < updateAvailable.Length; i++)
+            {
+                if (updateAvailable[i])
+                {
+                    try
+                    {
+                        client.DownloadFile(updateURL + fileNames[i], "Update/" + fileNames[i]); //.Replace(" ", "+")
+                    }
+                    catch { }
+                }
+            }
+
+            ExecuteScript(CreateScript());
+            Environment.Exit(0);
+        }
+
+        public static bool IsLinux
+        {
+            get
+            {
+                int p = (int)Environment.OSVersion.Platform;
+                return (p == 4) || (p == 6) || (p == 128);
+            }
+        }
+
+        private string CreateScript()
+        {
+            string updateFile = "update";
+            if (!IsLinux)
+                updateFile += ".bat";
+
+            client.DownloadFile(updateURL + updateFile, "Update/" + updateFile);
+            while (!File.Exists("Update/" + updateFile)) ;
+            return System.Environment.CurrentDirectory + "/Update/" + updateFile;
+        }
+
+        private void ExecuteScript(string script)
+        {
+            while (!File.Exists(script)) ;
+            Task t = new Task(
+                delegate 
+                {
+                    System.Environment.CurrentDirectory = "Update";
+                    System.Diagnostics.Process.Start(script);
+                });
+            t.Start();
+            t.Wait();
+        }
+    }
+}
